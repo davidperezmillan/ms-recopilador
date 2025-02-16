347a2cd967d54ed1c99efb1f6a83e31b15dad88d
David Perez Millan <david.perez.millan@gmail.com>
2025-02-16 14:11:08 +0100

    remove unused imports in EnabledController

--- src/main/java/com/davidperezmillan/recopilador/infrastructure/web/controller/EnabledController.java
+++ src/main/java/com/davidperezmillan/recopilador/infrastructure/web/controller/EnabledController.java
@@ -1,12 +1,9 @@
 package com.davidperezmillan.recopilador.infrastructure.web.controller;
 
-import com.davidperezmillan.recopilador.apllication.service.ScraperLauncherService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping()
