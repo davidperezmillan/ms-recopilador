440438906e8cc5d92bd4d95aa7733367953a0fa2
David Perez Millan <david.perez.millan@gmail.com>
2025-04-05 11:15:15 +0200

    Refactor MDCFilter to simplify request ID logging and remove unused code

--- src/main/java/com/davidperezmillan/recopilador/infrastructure/config/MDCFilter.java
+++ src/main/java/com/davidperezmillan/recopilador/infrastructure/config/MDCFilter.java
@@ -1,29 +1,7 @@
 package com.davidperezmillan.recopilador.infrastructure.config;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.MDC;
-import org.springframework.stereotype.Component;
+/**
+ * filtro para incluir el id de la peticion en el MDC
+ * para poder usarlo en los logs
+ */
 
-import java.io.IOException;
-import java.util.UUID;
-
-@Component
-public class MDCFilter implements Filter {
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        try {
-            // Asigna un identificador único para la petición
-            MDC.put("requestId", UUID.randomUUID().toString());
-            // Puedes extraer información adicional, por ejemplo, el IP del cliente:
-            HttpServletRequest httpRequest = (HttpServletRequest) request;
-            MDC.put("clientIP", httpRequest.getRemoteAddr());
-            MDC.put("applicationName", "recopilador");
-            chain.doFilter(request, response);
-        } finally {
-            MDC.clear();
-        }
-    }
-}
