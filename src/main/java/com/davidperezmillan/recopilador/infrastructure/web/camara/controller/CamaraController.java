4576cda9de58f8f8f08b63c90a784a1517544610
David Perez Millan <david.perez.millan@gmail.com>
2025-09-18 13:19:54 +0200

    Add endpoint to retrieve camera snapshot

--- src/main/java/com/davidperezmillan/recopilador/infrastructure/web/camara/controller/CamaraController.java
+++ src/main/java/com/davidperezmillan/recopilador/infrastructure/web/camara/controller/CamaraController.java
@@ -114,6 +114,28 @@ public class CamaraController {
                 });
     }
 
+    @GetMapping("snapshot/{camara}")
+    public Mono<byte[]> getSnapshot(@PathVariable String camara) {
+        String ipCamara = camaras.get(camara);
+        WebClient webClient = WebClient.builder()
+                .baseUrl(ipCamara)
+                .defaultHeader(HttpHeaders.ACCEPT, "application/json, text/javascript, */*; q=0.01")
+                .defaultHeader(HttpHeaders.ACCEPT_LANGUAGE, "es-ES,es;q=0.9")
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Basic ZGF2aWQ6Y2xvbjk4OTc=")
+                .defaultHeader(HttpHeaders.CONNECTION, "keep-alive")
+                .defaultHeader(HttpHeaders.REFERER, ipCamara + "/?page=eventsdir")
+                .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36")
+                .defaultHeader("X-Requested-With", "XMLHttpRequest")
+                .build();
+
+        return webClient.get()
+                .uri("/cgi-bin/snapshot.sh")
+                .accept(MediaType.IMAGE_JPEG)
+                .retrieve()
+                .bodyToMono(byte[].class);
+
+    }
+
     @GetMapping("/reset")
     public Mono<String> resetCamera() {
         WebClient webClient = WebClient.builder()
